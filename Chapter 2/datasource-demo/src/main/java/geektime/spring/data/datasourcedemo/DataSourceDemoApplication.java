412c0573ca4f206e75aecc36e91bb61a91681592
MaybeSilent <804758962@qq.com>
2021-03-21 11:00:43 +0800

    init repository with mirror

---
+++ Chapter 2/datasource-demo/src/main/java/geektime/spring/data/datasourcedemo/DataSourceDemoApplication.java
@@ -0,0 +1,46 @@
+package geektime.spring.data.datasourcedemo;
+
+// lombok简化代码
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+// SpringBootApplication注解
+@SpringBootApplication
+@Slf4j // slf4j注解
+public class DataSourceDemoApplication implements CommandLineRunner {
+	@Autowired // 自动注入springboot自动配置的dataSource，配置文件中有相关
+	private DataSource dataSource;
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataSourceDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		showConnection();
+		showData();
+	}
+
+	private void showConnection() throws SQLException {
+		log.info(dataSource.toString());
+		Connection conn = dataSource.getConnection();
+		log.info(conn.toString());
+		conn.close();
+	}
+
+	private void showData() {
+		jdbcTemplate.queryForList("SELECT * FROM FOO") // jdbcTemplate，Spring中最基础的访问数据库实现方式
+				.forEach(row -> log.info(row.toString()));
+	}
+}
+
